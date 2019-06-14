8ba67501d1372be3d4573da4198f9e55b7f108fd
qinwq08 <853466060@qq.com>
2019-06-14 16:06:28 +0800

    实验

--- src/com/first/hello.java
+++ src/com/first/hello.java
@@ -3,6 +3,6 @@ package com.first;
 public class hello {
     public static void main(String[] args) {
         System.out.println("123456hello");
-
+        System.out.println("123456hello");
     }
 }
