90912e58c271132fccae1cbcbf067cf5693b8d99
qinwq08 <853466060@qq.com>
2019-06-14 15:47:31 +0800

    Initial commit

---
+++ src/com/first/color.java
@@ -0,0 +1,12 @@
+package com.first;
+
+public class color {
+    private String color;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getColor() {
+        return color;
+    }
+}
---
+++ src/com/first/hello2.java
@@ -0,0 +1,7 @@
+package com.first;
+
+public class hello2 {
+    public static void main(String[] args) {
+        System.out.println("10086");
+    }
+}
